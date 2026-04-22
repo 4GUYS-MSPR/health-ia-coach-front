02ba839c0e89090ad1499b3882f540c7ca6e1347
Sébastien Lotten <sebastienlotten@proton.me>
2026-04-22 22:11:29 +0200

    Add theme cubit

--- lib/feature/theme/theme_cubit.dart
+++ lib/feature/theme/theme_cubit.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class ThemeCubit extends Cubit<ThemeMode>{
-  ThemeCubit() : super(ThemeMode.light);
+  static const String _themeKey= 'user_theme';
 
-  void toggleTheme(){
-    emit( state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
+  ThemeCubit() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final preference = await SharedPreferences.getInstance();
+    final savedIndex = preference.getInt(_themeKey);
+
+    if (savedIndex != null){
+      emit(ThemeMode.values[savedIndex]);
+    }
+
+  }
+
+  Future<void> toggleTheme() async {
+    final newMode = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    emit(newMode);
+
+    final preference = await SharedPreferences.getInstance();
+    await preference.setInt(_themeKey, newMode.index);
   }
 }
