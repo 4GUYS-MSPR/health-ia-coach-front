4237806e46f96e59c055ae504d1dd89eff42bec2
Sébastien Lotten <sebastienlotten@proton.me>
2026-04-23 20:00:21 +0200

    Modify go router file and add bottom bar

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -2,22 +2,42 @@ import 'package:go_router/go_router.dart';
 import 'package:health_ia_care/app/router/app_routes.dart';
 import 'package:health_ia_care/core/theme/shared/pages/error_page.dart';
 import 'package:health_ia_care/core/theme/shared/pages/home_page.dart';
+import 'package:health_ia_care/core/theme/shared/pages/app_view.dart';
+import 'package:health_ia_care/core/theme/shared/pages/profil_page.dart';
+
+
 
 class AppRouter {
   GoRouter get router => _goRouter;
 
   final _goRouter = GoRouter(
-    initialLocation: AppRoutes.root,
+    initialLocation: AppRoutes.home,
     errorBuilder: (context, state) => const ErrorPage(),
     routes: [
-      GoRoute(
-        path: AppRoutes.root, 
-        redirect: (context, state) => AppRoutes.home),
-      GoRoute(
-        path: AppRoutes.home,
-        name: AppRoutes.home,
-        builder: (context, state) => const HomePage(),
-      )
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+        return AppView(
+          navigationShell: navigationShell);
+      },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.home,
+                builder: (context, state) => const HomePage(),
+              )
+            ]
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.profil,
+                builder: (context, state) => const ProfilPage(),
+              )
+            ]
+          )
+        ]
+          )
     ]
   );
 }
