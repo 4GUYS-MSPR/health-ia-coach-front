661c393023741bb06a6c36c48da1fad38c3de6e4
DCodeProg <danael.legrand@outlook.fr>
2026-04-28 11:25:49 +0200

    refactor: update import paths for ThemeCubit

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../features/theme/theme_cubit.dart';
+import '../core/shared/cubits/theme_cubit/theme_cubit.dart';
 import 'router/app_router.dart';
 import 'service_locator/service_locator.dart';
 
