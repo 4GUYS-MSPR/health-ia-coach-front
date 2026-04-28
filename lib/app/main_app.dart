f2250813e29dd72b2f0efda61ef122e1944dd6f4
DCodeProg <danael.legrand@outlook.fr>
2026-04-28 11:26:33 +0200

    refactor: update ThemeCubit instantiation to use service locator

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ThemeCubit(),
+      create: (context) => sl<ThemeCubit>(),
       child: Builder(
         builder: (context) {
           final appRouter = sl<AppRouter>();
