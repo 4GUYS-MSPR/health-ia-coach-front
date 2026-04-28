66a162aa7a911f58ebd6503c7d2e2433c5ec42ba
DCodeProg <danael.legrand@outlook.fr>
2026-04-28 11:25:58 +0200

    refactor: add HydratedStorage initialization and ThemeCubit registration

--- lib/app/service_locator/service_locator_core.dart
+++ lib/app/service_locator/service_locator_core.dart
@@ -1,14 +1,18 @@
 import 'package:get_it/get_it.dart';
 
 import 'init/dotenv_init.dart';
+import 'init/hydrated_storage_init.dart';
 import 'init/logger_init.dart';
 import 'modules/router_module.dart';
+import 'modules/theme_cubit_module.dart';
 
 Future<void> registerCoreDependencies(GetIt sl) async {
   // Inits
   await initDotenv(sl);
   await initLogger(sl);
+  await initHydratedStorage();
 
   // Modules
   registerRouter(sl);
+  registerThemeCubit(sl);
 }
