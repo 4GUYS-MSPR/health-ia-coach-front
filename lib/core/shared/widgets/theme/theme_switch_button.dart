fc7d57ff3e20df17e7097b10fa144ad062405cf5
DCodeProg <danael.legrand@outlook.fr>
2026-05-19 15:15:26 +0200

    refactor: convert SwitchThemeButton to StatelessWidget and simplify state management

--- lib/core/shared/widgets/theme/theme_switch_button.dart
+++ lib/core/shared/widgets/theme/theme_switch_button.dart
@@ -1,45 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 import '../../../extensions/l10n_extension.dart';
 import '../../cubits/theme_cubit.dart';
 
-class SwitchThemeButton extends StatefulWidget {
+class SwitchThemeButton extends StatelessWidget {
   const SwitchThemeButton({super.key});
 
   @override
-  State<SwitchThemeButton> createState() => _SwitchThemeButtonState();
-}
-
-class _SwitchThemeButtonState extends State<SwitchThemeButton> {
-  @override
   Widget build(BuildContext context) {
-    final ThemeMode currentMode = context.watch<ThemeCubit>().state;
-    return SafeArea(
-      child: SegmentedButton<ThemeMode>(
-        emptySelectionAllowed: false,
-        segments: [
-          ButtonSegment(
-            value: ThemeMode.system,
-            label: Text(context.l10n.themeSystemName),
-            icon: Icon(Icons.settings_brightness_outlined),
-          ),
-          ButtonSegment(
-            value: ThemeMode.light,
-            label: Text(context.l10n.themeLightName),
-            icon: Icon(Icons.light_mode_outlined),
-          ),
-          ButtonSegment(
-            value: ThemeMode.dark,
-            label: Text(context.l10n.themeDarkName),
-            icon: Icon(Icons.dark_mode_outlined),
-          ),
-        ],
-        selected: {currentMode},
-        onSelectionChanged: (Set<ThemeMode> newSelection) {
-          context.read<ThemeCubit>().setTheme(newSelection.first);
-        },
-      ),
+    void onSelectionChanged(Set<ThemeMode> p1) {
+      context.read<ThemeCubit>().setTheme(p1.first);
+    }
+
+    return BlocBuilder<ThemeCubit, ThemeMode>(
+      builder: (context, state) {
+        return SegmentedButton<ThemeMode>(
+          emptySelectionAllowed: false,
+          showSelectedIcon: false,
+          segments: [
+            ButtonSegment(
+              value: .system,
+              label: Text(context.l10n.themeSystemName),
+              icon: Icon(Symbols.routine),
+            ),
+            ButtonSegment(
+              value: .light,
+              label: Text(context.l10n.themeLightName),
+              icon: Icon(Symbols.light_mode),
+            ),
+            ButtonSegment(
+              value: .dark,
+              label: Text(context.l10n.themeDarkName),
+              icon: Icon(Symbols.dark_mode),
+            ),
+          ],
+          selected: {state},
+          onSelectionChanged: onSelectionChanged,
+        );
+      },
     );
   }
 }
