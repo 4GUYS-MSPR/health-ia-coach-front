7e729e28f4c7590256c3bb812b948f51623f52d6
DCodeProg <danael.legrand@outlook.fr>
2026-05-19 16:44:02 +0200

    style: sort imports

--- lib/features/auth/domain/usecases/logout_usecase.dart
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:fpdart/fpdart.dart';
 
-import '../../../../core/usecases/usecase.dart';
 import '../../../../core/errors/failure.dart';
+import '../../../../core/usecases/usecase.dart';
 import '../repositories/auth_repository.dart';
 
 class LogoutUseCase implements UseCase<bool, NoParams> {
