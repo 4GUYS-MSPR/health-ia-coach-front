3eb7823ef5a1fde0eebe008e74bfcdeee7a7ad7f
DCodeProg <danael.legrand@outlook.fr>
2026-05-19 15:17:52 +0200

    refactor: update Failure class to remove interface modifier and improve formatting

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,16 +1,22 @@
 import 'package:equatable/equatable.dart';
 
-abstract interface class Failure extends Equatable {
+abstract class Failure extends Equatable {
   final String message;
 
-  const Failure({required this.message});
+  const Failure({
+    required this.message,
+  });
+
+  @override
+  List<Object?> get props => [
+    message,
+  ];
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure({required super.message});
-
-  @override
-  List<Object?> get props => [];
+  const ServerFailure({
+    required super.message,
+  });
 }
 
 class AiFailure extends Failure {
